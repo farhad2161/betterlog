c47fd34815a8afaf34d28605b94724150a81fd1f
farhad2161 <farhad2161@yahoo.com>
2017-10-14 17:57:06 +0330

    Adding button for create some dummy log.

--- app/src/main/java/com/kia/betterlogapp/MainActivity.java
+++ app/src/main/java/com/kia/betterlogapp/MainActivity.java
@@ -2,8 +2,11 @@ package com.kia.betterlogapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.kia.betterlog.Log;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("I am a label", "I am a message");
+    }
+
+    public void writeDummyLog(View view) {
+        Log.setTag("AN OPTIONAL TAG");
+        Log.d("I am a label", "I am debug message");
+        Log.e(Arrays.asList("label1", "label2"), "I am error message.");
+        Log.w("I am warning message.");
+        Log.i("Info!!", "I am info message");
+
     }
 }
